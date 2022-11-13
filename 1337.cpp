f6725dda5dc44eafa293c38de4bc1bb33ec2d622
MrGogoll <115408973+MrGogoll@users.noreply.github.com>
2022-11-13 15:13:50 +0300

    18

---
+++ 1337.cpp
@@ -0,0 +1,22 @@
+﻿// 
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c,D;
+    cin >> a >> b >> c;
+    D = b * b - 4 * a * c;
+    if (D == 0) {
+        cout << 1;
+    }
+    if (D > 0) {
+        cout << 2;
+    }
+    if (D < 0) {
+        cout << 0;
+    }
+
+
+}
+
